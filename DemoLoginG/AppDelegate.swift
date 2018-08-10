cfea601454d46c47f52fe9de076b4e92675999a6
Đừng xóa <nomnom1204@gmail.com>
2018-08-10 08:16:20 +0700

    init

--- DemoLoginG/AppDelegate.swift
+++ DemoLoginG/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GIDSignIn.sharedInstance().clientID = "170080066131-khaa7m0b84bn09hdtcct43g95bdtnemu.apps.googleusercontent.com"
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url as URL?, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- DemoLoginG/ViewController.swift
+++ DemoLoginG/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
+    @IBOutlet weak var avarImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    var dispatchWorkItem: DispatchWorkItem?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        
+//        let googleSignInButton = GIDSignInButton()
+//        googleSignInButton.center = view.center
+//        view.addSubview(googleSignInButton)
     }
 
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if error != nil {
+            print(error ?? "google error")
+            return
+        }
+        
+        emailLabel.text = user.profile.email
+        if user.profile.hasImage == true {
+            let imageUrl = user.profile.imageURL(withDimension: 120)
+            print(imageUrl!)
+            loadViewIfNeeded()
+            getImage(from: "\(imageUrl!)", completedHandler: { (image) in
+                self.avarImage.image = image
+            })
+        }
+    }
+    
+    
+    @IBAction func ss(_ sender: AnyObject) {
+        GIDSignIn.sharedInstance().signOut()
+        if GIDSignIn.sharedInstance().currentUser == nil {
+            emailLabel.text = "No data"
+            avarImage.image = #imageLiteral(resourceName: "NoPhoto")
+            view.reloadInputViews()
+        }
+    }
+    
+    func getImage(from urlString: String, completedHandler: @escaping (UIImage?) -> Void) {
+        guard let url = URL(string: urlString) else {return}
+        var image: UIImage?
+        dispatchWorkItem = DispatchWorkItem(block: {
+            if let data = try? Data(contentsOf: url) {
+                image = UIImage(data: data)
+            }
+        })
+        DispatchQueue.global().async {
+            self.dispatchWorkItem?.perform()
+            DispatchQueue.main.async {
+                completedHandler(image)
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
